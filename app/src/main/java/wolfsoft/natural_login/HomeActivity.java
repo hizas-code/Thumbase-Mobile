961cb43c330ec1ee7df91cae734052a12539d17c
hizas-code <hizassr@gmail.com>
2017-02-09 01:02:40 +0700

    search box 1%

--- app/src/main/java/wolfsoft/natural_login/HomeActivity.java
+++ app/src/main/java/wolfsoft/natural_login/HomeActivity.java
@@ -2,6 +2,7 @@ package wolfsoft.natural_login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+
     }
 }
